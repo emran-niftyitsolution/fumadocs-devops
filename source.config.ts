6b651f14c4700c73855c3495ec6c403275d012ab
emran-niftyitsolution <emran@niftyitsolution.com>
2025-07-21 11:58:06 +0600

    Refactor configuration files and improve syntax consistency across various documents. Updated PostCSS and source configuration exports, standardized import statements in global CSS, and adjusted formatting in Ansible inventory sections for better readability.

--- source.config.ts
+++ source.config.ts
@@ -1,9 +1,4 @@
-import {
-  defineConfig,
-  defineDocs,
-  frontmatterSchema,
-  metaSchema,
-} from 'fumadocs-mdx/config';
+import { defineDocs, frontmatterSchema, metaSchema } from "fumadocs-mdx/config";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
@@ -13,11 +8,5 @@ export const docs = defineDocs({
   },
   meta: {
     schema: metaSchema,
-  },
-});
-
-export default defineConfig({
-  mdxOptions: {
-    // MDX options
   },
 });
